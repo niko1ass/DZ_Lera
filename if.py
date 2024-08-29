5e6b9f39d49b1fcaca231eec62c56fea1c0946f8
niko1ass <nik2006ri@mail.ru>
2024-08-29 21:10:52 +0300

    Добавил третью переменную 'c'

--- if.py
+++ if.py
@@ -1,19 +1,20 @@
 w = input("введите знак (+, -, *, /): ")
 
 a = float(input("введитие первое число: "))
-b = float(input("введитие первое число: "))
+b = float(input("введитие второе число: "))
+с = float(input("введитие третье число: "))
 
 if w == "+":
-	c = a + b
+	c = a + b + с
 	print("Результат: " + str(c))
 elif w == "-":
-	c = a - b
+	c = a - b - с
 	print("Результат: " + str(c))
 elif w == "*":
-	c = a * b
+	c = a * b * с
 	print("Результат: " + str(c))
 elif w == "/":
-	c = a / b
+	c = a / b / с
 	print("Результат: " + str(c))
 else:
 	print("ERROR")
